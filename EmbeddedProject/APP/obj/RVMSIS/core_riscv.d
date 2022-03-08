74d9842349fb6193f68f91043ee4bd204ffa4ec5
kaidegit <2857693944@qq.com>
2022-03-08 22:00:53 +0800

    add template project

---
+++ EmbeddedProject/APP/obj/RVMSIS/core_riscv.d
@@ -0,0 +1,2 @@
+RVMSIS/core_riscv.o: \
+ C:/Users/yekai/Documents/GitHub/ComputerOccupyingMonitor/EmbeddedProject/SRC/RVMSIS/core_riscv.c
---
+++ EmbeddedProject/APP/obj/StdPeriphDriver/CH57x_pwm.d
@@ -0,0 +1,50 @@
+StdPeriphDriver/CH57x_pwm.o: \
+ C:/Users/yekai/Documents/GitHub/ComputerOccupyingMonitor/EmbeddedProject/SRC/StdPeriphDriver/CH57x_pwm.c \
+ C:\Users\yekai\Documents\GitHub\ComputerOccupyingMonitor\EmbeddedProject\SRC\StdPeriphDriver\inc/CH57x_common.h \
+ C:\Users\yekai\Documents\GitHub\ComputerOccupyingMonitor\EmbeddedProject\SRC\StdPeriphDriver\inc/CH573SFR.h \
+ C:\Users\yekai\Documents\GitHub\ComputerOccupyingMonitor\EmbeddedProject\SRC\RVMSIS/core_riscv.h \
+ C:\Users\yekai\Documents\GitHub\ComputerOccupyingMonitor\EmbeddedProject\SRC\StdPeriphDriver\inc/CH57x_clk.h \
+ C:\Users\yekai\Documents\GitHub\ComputerOccupyingMonitor\EmbeddedProject\SRC\StdPeriphDriver\inc/CH57x_uart.h \
+ C:\Users\yekai\Documents\GitHub\ComputerOccupyingMonitor\EmbeddedProject\SRC\StdPeriphDriver\inc/CH57x_gpio.h \
+ C:\Users\yekai\Documents\GitHub\ComputerOccupyingMonitor\EmbeddedProject\SRC\StdPeriphDriver\inc/CH57x_flash.h \
+ C:\Users\yekai\Documents\GitHub\ComputerOccupyingMonitor\EmbeddedProject\SRC\StdPeriphDriver\inc/CH57x_pwr.h \
+ C:\Users\yekai\Documents\GitHub\ComputerOccupyingMonitor\EmbeddedProject\SRC\StdPeriphDriver\inc/CH57x_pwm.h \
+ C:\Users\yekai\Documents\GitHub\ComputerOccupyingMonitor\EmbeddedProject\SRC\StdPeriphDriver\inc/CH57x_adc.h \
+ C:\Users\yekai\Documents\GitHub\ComputerOccupyingMonitor\EmbeddedProject\SRC\StdPeriphDriver\inc/CH57x_sys.h \
+ C:\Users\yekai\Documents\GitHub\ComputerOccupyingMonitor\EmbeddedProject\SRC\StdPeriphDriver\inc/CH57x_timer.h \
+ C:\Users\yekai\Documents\GitHub\ComputerOccupyingMonitor\EmbeddedProject\SRC\StdPeriphDriver\inc/CH57x_spi.h \
+ C:\Users\yekai\Documents\GitHub\ComputerOccupyingMonitor\EmbeddedProject\SRC\StdPeriphDriver\inc/CH57x_usbdev.h \
+ C:\Users\yekai\Documents\GitHub\ComputerOccupyingMonitor\EmbeddedProject\SRC\StdPeriphDriver\inc/CH57x_usbhost.h \
+ C:\Users\yekai\Documents\GitHub\ComputerOccupyingMonitor\EmbeddedProject\SRC\StdPeriphDriver\inc/ISP573.h
+
+C:\Users\yekai\Documents\GitHub\ComputerOccupyingMonitor\EmbeddedProject\SRC\StdPeriphDriver\inc/CH57x_common.h:
+
+C:\Users\yekai\Documents\GitHub\ComputerOccupyingMonitor\EmbeddedProject\SRC\StdPeriphDriver\inc/CH573SFR.h:
+
+C:\Users\yekai\Documents\GitHub\ComputerOccupyingMonitor\EmbeddedProject\SRC\RVMSIS/core_riscv.h:
+
+C:\Users\yekai\Documents\GitHub\ComputerOccupyingMonitor\EmbeddedProject\SRC\StdPeriphDriver\inc/CH57x_clk.h:
+
+C:\Users\yekai\Documents\GitHub\ComputerOccupyingMonitor\EmbeddedProject\SRC\StdPeriphDriver\inc/CH57x_uart.h:
+
+C:\Users\yekai\Documents\GitHub\ComputerOccupyingMonitor\EmbeddedProject\SRC\StdPeriphDriver\inc/CH57x_gpio.h:
+
+C:\Users\yekai\Documents\GitHub\ComputerOccupyingMonitor\EmbeddedProject\SRC\StdPeriphDriver\inc/CH57x_flash.h:
+
+C:\Users\yekai\Documents\GitHub\ComputerOccupyingMonitor\EmbeddedProject\SRC\StdPeriphDriver\inc/CH57x_pwr.h:
+
+C:\Users\yekai\Documents\GitHub\ComputerOccupyingMonitor\EmbeddedProject\SRC\StdPeriphDriver\inc/CH57x_pwm.h:
+
+C:\Users\yekai\Documents\GitHub\ComputerOccupyingMonitor\EmbeddedProject\SRC\StdPeriphDriver\inc/CH57x_adc.h:
+
+C:\Users\yekai\Documents\GitHub\ComputerOccupyingMonitor\EmbeddedProject\SRC\StdPeriphDriver\inc/CH57x_sys.h:
+
+C:\Users\yekai\Documents\GitHub\ComputerOccupyingMonitor\EmbeddedProject\SRC\StdPeriphDriver\inc/CH57x_timer.h:
+
+C:\Users\yekai\Documents\GitHub\ComputerOccupyingMonitor\EmbeddedProject\SRC\StdPeriphDriver\inc/CH57x_spi.h:
+
+C:\Users\yekai\Documents\GitHub\ComputerOccupyingMonitor\EmbeddedProject\SRC\StdPeriphDriver\inc/CH57x_usbdev.h:
+
+C:\Users\yekai\Documents\GitHub\ComputerOccupyingMonitor\EmbeddedProject\SRC\StdPeriphDriver\inc/CH57x_usbhost.h:
+
+C:\Users\yekai\Documents\GitHub\ComputerOccupyingMonitor\EmbeddedProject\SRC\StdPeriphDriver\inc/ISP573.h:
